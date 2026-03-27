86e9469ceb2610f952934526afecc6dd552a29e9
maisvendoo <maisvendoo@gmail.com>
2026-03-27 13:51:43 +0300

    Некторорые правки в графике

--- routes/rostovskij_uzel/scenarios/train_259C/main.lua
+++ routes/rostovskij_uzel/scenarios/train_259C/main.lua
@@ -1,6 +1,7 @@
 setTime("08:09:30")
 --setTime("09:30")
 
+-- vehicle #0
 train1 = TrainData.new()
 train1.name = "259C"
 train1.config = "ep1m-T471_16"
@@ -10,6 +11,7 @@ train1.dir = -1
 train1.auto = true
 setTrain(train1)
 
+-- vehicle #17
 train2 = TrainData.new()
 train2.name = "259A"
 train2.config = "ep1m-T471_16"
@@ -19,6 +21,7 @@ train2.dir = 1
 train2.auto = true
 setTrain(train2)
 
+-- vehicle #34
 train3 = TrainData.new()
 train3.name = "134"
 train3.config = "vl60pk-1543-T65_17"
@@ -29,6 +32,7 @@ train3.dir = 1
 train3.auto = true
 setTrain(train3)
 
+-- vehicle #52
 train4 = TrainData.new()
 train4.name = "6813"
 train4.config = "ra3-024"
@@ -38,6 +42,7 @@ train4.dir = -1
 train4.auto = true
 setTrain(train4)
 
+-- vehicle #54
 train5 = TrainData.new()
 train5.name = "309"
 train5.config = "vl60pk-1543-T65_17"
@@ -47,6 +52,7 @@ train5.dir = -1
 train5.auto = true
 setTrain(train5)
 
+-- vehicle #72
 train6 = TrainData.new()
 train6.name = "2004"
 train6.config = "vl60k-1737-frEmpties"
@@ -56,6 +62,7 @@ train6.dir = 1
 train6.auto = true
 setTrain(train6)
 
+-- -- vehicle #123
 train7 = TrainData.new()
 train7.name = "2005"
 train7.config = "vl60k-1737-frEmpties"
